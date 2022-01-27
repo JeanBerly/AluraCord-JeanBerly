16c951dfb64c7eff6e9e53897765e3cc527fe4a8
JeanBerly <jeanberlyy@gmail.com>
2022-01-27 11:25:25 -0300

    Feito os desafios da aula 3 e a estilização das mensagens

--- pages/chat.js
+++ pages/chat.js
@@ -1,18 +1,64 @@
 import configs from '../config.json';
 import React from 'react';
 const colors = configs.theme.colors;
-export default function chatPage(){
+export default function chatPage() {
+    const idMsg = 0;
     const [message, setMessage] = React.useState('');
     const [chatHistory, setChatHistory] = React.useState([]);
-    let bloh = chatHistory.map((msg) =>{
-        return(
-            <div>{msg.user}: {msg.text}</div>
+    let bloh = chatHistory.map((msg) => {
+        const imgGit = `https://github.com/${msg.user}.png`;
+        return (
+            <>
+                <div key={msg.id} >
+                    <img src={imgGit}></img><span>{msg.user} {msg.date}</span>
+                    <button value={msg.id} onClick={(event) => {
+                        let aux = chatHistory.filter((msg) => {
+                            return (!((msg.id) == event.target.value));
+                        });
+                        setChatHistory([
+                            ...aux,
+                        ]);
+                    }} >X</button>
+                    <p>{msg.text}</p>
+                </div>
+                <style jsx>{`
+                    div{
+                        display: block;
+                        position: relative;
+                        height: 10%;
+                        min-height: fit-content;
+                    }
+                    img{
+                        position: relative;
+                        height: 2rem;
+                        border-radius: 50%;
+                    }
+                    span{
+                        position: absolute;
+                        top: 0.5rem;
+                        overflow-y: auto;
+                    }
+                    p{
+                        position: relative;
+                        min-height: fit-content;
+                    }
+                    button{
+                        display: block;
+                        width: 30px;
+                        height: 30px;
+                        position: absolute;
+                        top: 0;
+                        right: 0;
+                        background-color: red;
+                    }
+                `}</style>
+            </>
         )
     });
-    function handleNewMessage(newMessage){
+    function handleNewMessage(newMessage) {
         const message = {
-            id: chatHistory.length + 1,
-            user: 'Jean',
+            id: Date.now(),
+            user: 'JeanBerly',
             text: newMessage,
             date: '15/12/2022'
         }
@@ -21,25 +67,26 @@ export default function chatPage(){
             message
         ]);
     }
-    return(
+    return (
         <>
             <ChatBox>
                 <Header></Header>
                 <Chat chatHistory={chatHistory}>
-                    Msgs: {bloh}
+                    {bloh}
                 </Chat>
                 <Footer handleNewMessage={handleNewMessage}></Footer>
             </ChatBox>
         </>
     )
 }
-function ChatBox(props){
+function ChatBox(props) {
     return (
         <>
             <div>{props.children}</div>
             <style jsx>{`
                 div{
                     display: flex;
+                    position: absolute;
                     align-items: center;
                     position: relative;
                     flex-direction: column;
@@ -51,7 +98,7 @@ function ChatBox(props){
         </>
     )
 }
-function Header(props){
+function Header(props) {
     return (
         <>
             <header>
@@ -88,13 +135,14 @@ function Header(props){
         </>
     )
 }
-function Chat(props){
-    return(
+function Chat(props) {
+    return (
         <>
             <main>{props.children}</main>
             <style jsx>{`
                 main{
                     display: flex;
+                    position: relative;
                     flex-direction: column;
                     color: white;
                     width: 95%;
@@ -102,40 +150,40 @@ function Chat(props){
                     background-color: ${colors.neutrals[700]};
                     border-radius: 20px;
                     margin: auto 0;
+                    min-height: fit-content;
+                    overflow-y: auto;
                 }
             `}</style>
         </>
     )
 }
-// onChange={(event) =>{
-//     const msg = event.target.value;
-//     if (msg.charAt(msg.length - 1) == 13){
-//         console.log('blohzada');
-//     }
-// }}
-function Footer(props){
-    return(
+function Footer(props) {
+    return (
         <>
             <footer>
-                <input type="text" placeholder="Enter your message here" onKeyPress={(event) =>{
-                    if (event.key == 'Enter'){
-                        const newMessage = event.target.value;
-                        props.handleNewMessage(newMessage);
+                <input id="textoMsg" type="text" placeholder="Enter your message here" onKeyPress={(event) => {
+                    if (event.key == 'Enter') {
+                        const str = event.target.value
+                        if (!(str.length == 0)) {
+                            const newMessage = event.target.value;
+                            props.handleNewMessage(newMessage);
+                        }
                         event.target.value = '';
-                        // props.setChatHistory([
-                        //     ...props.chatHistory,
-                        //     event.target.value
-                        // ]);
-                        // event.target.value = '';
-                        // pegar a mensagem e colocar no chat, verificar se a msg n esta vazia
                     }
                 }} ></input>
-                <div id="buttonEmojis"></div>
+                <button onClick={() =>{
+                    const msg = document.getElementById("textoMsg");
+                    if (!(msg.value.length == 0)){
+                        props.handleNewMessage(msg.value);
+                        msg.value = '';
+                    }
+                }} id="buttonEmojis">Send</button>
             </footer>
             <style jsx>{`
                 footer{
                     display: flex;
-                    position: relative;
+                    position: sticky;
+                    bottom: 0;
                     justify-content: space-around;
                     align-items: center;
                     width: 95%;
@@ -146,20 +194,22 @@ function Footer(props){
                     display: block;
                     margin-right: auto;
                     color: white;
-                    font-size: 2rem;
+                    font-size: 1.5rem;
                     width: 90%;
                     height: 100%;
                     border-radius: 20px;
                     background-color: ${colors.neutrals[900]};
+                    overflow-wrap: scroll;
                 }
                 #buttonEmojis{
                     display: block;
                     position: relative;
-                    width: 80px;
-                    height: 80px;
+                    width: calc(15px + 3.5vmax);
+                    height: calc(15px + 3.5vmax);
                     margin-right: auto;
                     border-radius: 50%;
-                    background-color: white;
+                    color: black;
+                    font-size: calc(7.5px + 1vmax);
                 }
                 #buttonEmojis:hover{
                     cursor: pointer;
